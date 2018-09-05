5f4b53e3b7d02f31cadca28af2985a890b2ed8ae
modern <42424318+moderndx@users.noreply.github.com>
2018-09-04 17:17:10 -0700

    Update sv_action_cmds.lua

--- modern-action-commands/lua/autorun/server/sv_action_cmds.lua
+++ modern-action-commands/lua/autorun/server/sv_action_cmds.lua
@@ -6,17 +6,14 @@ util.AddNetworkString("modern_timeout")
 local timeout_players = {}
 
 local function get_table_name(ply)
-  if (game.SinglePlayer()) then
-    return ply:EntIndex()
-  end
-  return ply:SteamID64()
+  return game.SinglePlayer() and ply:EntIndex() or ply:SteamID64()
 end
 
 local function player_on_timeout(player, command_n)
   if !timeout_players[command_n] then timeout_players[command_n] = {} end
   local m_p = get_table_name(player)
 
-  for k,v in _G["pairs"](timeout_players[command_n]) do
+  for k,v in pairs(timeout_players[command_n]) do
     if (m_p == v) then return true end
   end
 
@@ -26,7 +23,7 @@ end
 local function saved_val_on_timeout(saved_id, command_n)
   if !timeout_players[command_n] then timeout_players[command_n] = {} end
 
-  for k,v in _G["pairs"](timeout_players[command_n]) do
+  for k,v in pairs(timeout_players[command_n]) do
     if (saved_id == v) then return true end
   end
 
@@ -50,13 +47,13 @@ local function handle_timeout(command_n, timeout_amt, player)
 end
 
 local function command_exists_in_config(message_cmd)
-  for k,v in _G["pairs"](modern_action_commands_config.commands_output) do
+  for k,v in pairs(modern_action_commands_config.commands_output) do
     if (message_cmd == "/"..v.command) then return true end
   end
 end
 
 local function find_action_tbl(message_cmd)
-  for k, v in _G["pairs"](modern_action_commands_config.commands_output) do
+  for k, v in pairs(modern_action_commands_config.commands_output) do
     if (message_cmd == "/"..v.command) then return v end
   end
 end
@@ -67,18 +64,18 @@ local function check_command(ply, text, public)
     action_tbl["player_name"] = ply:GetName()
 
     if (action_tbl["timeout_amount"] != 0 && player_on_timeout(ply, action_tbl["command"])) then
-      _G["net"]["Start"]("modern_timeout")
-      _G["net"]["WriteTable"](action_tbl)
-      _G["net"]["Send"](ply)
+      net.Start("modern_timeout")
+      net.WriteTable(action_tbl)
+      net.Send(ply)
       if (modern_action_commands_config.hide_message) then return "" end
       return
     end
 
     handle_timeout(action_tbl["command"], action_tbl["timeout_amount"], ply)
 
-    _G["net"]["Start"]("modern_action_cmd")
-    _G["net"]["WriteTable"](action_tbl)
-    _G["net"]["Broadcast"]()
+    net.Start("modern_action_cmd")
+    net.WriteTable(action_tbl)
+    net.Broadcast()
 
     if (modern_action_commands_config.hide_message) then return "" end
   end
